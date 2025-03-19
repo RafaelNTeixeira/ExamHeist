bdac49712a8736970ab6084677f458c35443c4b1
RafaelNTeixeira <rafael2003t.18@gmail.com>
2025-03-19 20:48:19 +0000

    Fixed bug where after final cutscene pause menu wouldnt work

--- Assets/Scripts/ChangeSceneOnTimer.cs
+++ Assets/Scripts/ChangeSceneOnTimer.cs
@@ -13,13 +13,13 @@ public class ChangeSceneOnTimer : MonoBehaviour
 
         // Press the space key to skip the cutscene
         if (Input.GetKeyUp(KeyCode.Space)) {
-            GameManager.instance.canPauseGame = true && !isFinalCutscene; // Can only pause the game after initial cutscene
+            GameManager.instance.canPauseGame = true; // Can only pause the game after initial cutscene
             SceneManager.LoadScene(isFinalCutscene ? "MainMenu" : "GameScene");
         }
         
         if (changeTime <= 0) 
         {
-            GameManager.instance.canPauseGame = true && !isFinalCutscene; // Can only pause the game after initial cutscene
+            GameManager.instance.canPauseGame = true; // Can only pause the game after initial cutscene
             SceneManager.LoadScene(isFinalCutscene ? "MainMenu" : "GameScene");
         } 
     }
